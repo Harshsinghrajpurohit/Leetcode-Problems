a32e020d04960840d5ef462499004eb4fa273fcc
Harsh Singh Rajpurohit <harshrajpurohit412005@gmail.com>
2026-08-20 10:40:47 +0530

    Time: 3 ms (84.95%), Space: 21.2 MB (18.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-      ans=-1
-      count=0
-      for num in nums:
-        if count==0:
-            ans=num
-        if ans==num:
-            count+=1
-        else:
-            count-=1
-      return ans
+
+        ans = -1       # Current candidate
+        count = 0      # Candidate's current vote balance
+
+        for num in nums:
+
+            # If count becomes 0,
+            # choose the current number as the new candidate
+            if count == 0:
+                ans = num
+
+            # Same as candidate → candidate gets +1 vote
+            if ans == num:
+                count += 1
+
+            # Different from candidate → cancel one vote
+            else:
+                count -= 1
+
+        return ans
